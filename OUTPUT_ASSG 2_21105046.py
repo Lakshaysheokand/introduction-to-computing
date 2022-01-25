606b9ab1a21144077b23809a422a891eb6242a70
Lakshaysheokand <96582208+Lakshaysheokand@users.noreply.github.com>
2022-01-25 18:28:19 +0530

    Add files via upload

---
+++ OUTPUT_ASSG 2_21105046.py
@@ -0,0 +1,71 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: D:\PEC\PYTHON\assignment 2\ASSIGNMENT 2.py =============
+----------------------------------ASSIGNMENT 2-----------------------------
+-------------------------------Question #1----------------------------------
+
+Part a 
+Length of Input String = 35
+
+Part b
+egaugnal evitisnes esac a si nohtyP
+
+Part c
+a case sensitive
+
+Part d
+Python is object oriented language
+
+Part e
+10
+
+Part f
+Pythonisacasesensitivelanguage
+
+---------------------------------Question #2----------------------------------
+ 
+TO PRINT A STATEMENT IN GIVEN FORMAT.
+ 
+ Hey, Lakshay Sheokand here! 
+ My SID is 21105046 
+ I am from ECE and my CGPA is 10
+
+---------------------------------Question #3----------------------------------
+ 
+TO CALCULATE THE FOLLOWING USING BITWISE OPERATORS.
+ 
+ a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>2 : 0
+
+---------------------------------Question #4----------------------------------
+ 
+TO DETERMINE THE GREATEST NUMBER OF ALL THREE NUMBERS.
+ 
+Enter the first number :- 23
+Enter the second number:- 34
+Enter the third number :- 23
+Greatestnumber = 34
+
+---------------------------------QUESTION #5----------------------------------
+ 
+TO CHECK IF THE WORD name IS PRESENT IN THE STRING 
+ 
+Enter the string:- what is your name?
+Yes,name is there.
+ 
+ 
+----------------------------------QUESTION #6---------------------------------
+ 
+TO FIND IF THE TRIANGLE IS POSSIBLE OR NOT 
+ 
+Enter the FIRST side of the triangle:- 12
+Enter the SECOND side of the triangle:- 13
+Enter the THIRD side of the triangle:- 15
+ 
+YES,THE TRIANGLE IS POSSIBLE
+
+----------------------------------ASSIGNMENT OVER-----------------------------
